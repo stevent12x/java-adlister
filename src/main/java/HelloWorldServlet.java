ee02c6997f2d8d0631d1a22aa428c3e8a6603460
Steven Turner <steven.turner12x@gmail.com>
2019-08-20 14:51:07 -0500

    finished bonuses

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -9,9 +9,13 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
-                res.setContentType("text/html");
-                PrintWriter out = res.getWriter();
-                out.println("<h1>Hello, World!</h1>");
+        String name = req.getParameter("name");
+        System.out.println("Welcome " + name + "!");
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello, " + name + "!</h1>");
+
     }
 }
 
