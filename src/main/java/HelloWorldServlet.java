07fcd68e92a10aafb59e4b0e2668485411362321
Steven Turner <steven.turner12x@gmail.com>
2019-08-20 13:11:09 -0500

    finished servlet ex

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,16 @@
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+                res.setContentType("text/html");
+                PrintWriter out = res.getWriter();
+                out.println("<h1>Hello, World!</h1>");
     }
 }
