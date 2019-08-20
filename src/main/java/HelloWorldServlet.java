9afe19ff279a26ad1720cf7662973d5c8660c86e
Steven Turner <steven.turner12x@gmail.com>
2019-08-20 15:00:40 -0500

    added page counter

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -7,15 +7,17 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
+    int counter = 0;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
         String name = req.getParameter("name");
         System.out.println("Welcome " + name + "!");
 
         res.setContentType("text/html");
+        counter +=1;
         PrintWriter out = res.getWriter();
-        out.println("<h1>Hello, " + name + "!</h1>");
-
+        out.println("<h1>Hello, " + name + "!</h1><br><h4>Visits: " + counter + "</h4>");
     }
 }
 
