5754015b6fabf749f25845dc179adb90cf40c316
Steven Turner <steven.turner12x@gmail.com>
2019-08-22 15:02:44 -0500

    first commit - mvc exs

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,21 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+        import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+
+        <%=req.getParameter("username");%>
+        req.getParameter("password");%>
+         <c:if test="${param.username.equalsIgnoreCase('admin') && param.password.equalsIgnoreCase('password')}">
+        <c:redirect url="profile.jsp"></c:redirect>
+    </c:if>
+    }
+}
